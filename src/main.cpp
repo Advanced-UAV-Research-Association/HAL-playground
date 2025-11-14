881018642cf1c15fea1c54ad28e888fc2e002d89
aaronkoc <148407724+aaronkoc@users.noreply.github.com>
2025-11-14 13:19:28 -0800

    Base Code

---
+++ src/main.cpp
@@ -0,0 +1,110 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+//MPU6050 and Arduino Tutorial
+//https://www.youtube.com/watch?v=wTfSfhjhAU0
+//
+//Arduino Nano use A4(SDA), A5(SCL)
+volatile bool sendHigh;
+volatile byte sendLow = 0;
+const int MPU_ADDR = 0x68;
+volatile int16_t spi_buffer = 0;
+
+volatile int16_t gyro_x, gyro_y, gyro_z;
+volatile int16_t accel_x, accel_y, accel_z;
+volatile int16_t temp;
+
+char  tmp_str[7];
+
+char* convert_int16_to_str(int16_t i)
+{
+sprintf(tmp_str, "%6d", i);// Function to convert numerical values into Raw Data
+return tmp_str;
+}
+void setup() 
+{
+  Serial.begin(9600);//Change this to make data processing faster
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x1B);
+  Wire.write(0x08); //Changes sensitivity of the Gyroscope: look at register sheet
+  Wire.write(0x10);
+  Wire.endTransmission(true);
+  SPI.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE1));
+  pinMode(MISO, OUTPUT);
+  pinMode(MOSI, INPUT);
+  pinMode(SCK, INPUT);
+  pinMode(SS, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  SPCR |= _BV(SPE) | _BV(SPIE);// Enable SPI in slave mode
+  sei();
+  sendHigh=true;
+}
+ISR(SPI_STC_vect)
+{
+  byte dummy = SPDR;
+  int16_t v = accel_x;
+
+
+    if (sendHigh)
+    {
+      SPDR = (v >> 8) & 0xFF;
+      sendHigh = false;
+    }
+    else
+    {
+      SPDR = v & 0xFF;  
+      sendHigh = true;
+    }
+  //testing code
+}
+
+void loop() 
+{
+  //MPU6050 Register Sheet for specific register allocations
+  //https://cdn.sparkfun.com/datasheets/Sensors/Accelerometers/RM-MPU-6000A.pdf
+
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); //Select starting Acceleration Register
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 6, true); //Read next 6 registers
+  //Top register is High, bottom register is Low hence the bit shift
+  accel_x=4660;
+  //accel_x = Wire.read()<<8 | Wire.read();
+  //accel_y = Wire.read()<<8 | Wire.read();
+  accel_y=22136;
+  //accel_z = Wire.read()<<8 | Wire.read();
+  accel_z=39612;
+
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x43); //Select starting Gyroscope Register
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 6, true); //Read next 6 registers
+  //Top register is High, bottom register is Low hence the bit shift
+  //gyro_x = Wire.read()<<8 | Wire.read();
+  //gyro_y = Wire.read()<<8 | Wire.read();
+  //gyro_z = Wire.read()<<8 | Wire.read();
+  gyro_x=57072;
+  gyro_y=9029;
+  gyro_z=26505;
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x41); //Select starting Temperature Register
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 2, true); //Read next 2 registers
+  temp = Wire.read()<<8 | Wire.read();
+  Serial.print("aX = "); Serial.print (convert_int16_to_str(accel_x));//Print out acceleration data
+  Serial.print(" | aY = "); Serial.print (convert_int16_to_str(accel_y));
+  Serial.print(" | aZ = "); Serial.print (convert_int16_to_str(accel_z));
+//First part prints out the data format. The second converts values to Raw Data
+  Serial.print(" | gX = "); Serial.print (convert_int16_to_str(gyro_x));//Print out gyroscope data
+  Serial.print(" | gY = "); Serial.print (convert_int16_to_str(gyro_y));
+  Serial.print(" | gZ = "); Serial.print (convert_int16_to_str(gyro_z));
+
+  Serial.print(" | tmp = "); Serial.print (temp/340.00+36.65);//Print out Temperatrue data
+  Serial.println();
+} 
